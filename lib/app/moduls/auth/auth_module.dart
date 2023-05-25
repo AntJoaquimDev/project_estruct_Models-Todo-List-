55010b2f3d01b302da8a1d90a69f2eff47bd7c09
joaquim <a.joaquimsfilho@gmail.com>
2023-05-25 18:12:01 -0300

    correcao no path moduls

---
+++ lib/app/moduls/auth/auth_module.dart
@@ -0,0 +1,19 @@
+import 'package:provider/provider.dart';
+import 'package:todo_list_provider/app/core/modules/todo_list_modules.dart';
+import 'package:todo_list_provider/app/moduls/auth/login/login_controller.dart';
+import 'package:todo_list_provider/app/moduls/auth/login/login_page.dart';
+
+class AuthModule extends TodoListModules {
+  AuthModule()
+      : super(
+          bindings: [
+            ChangeNotifierProvider(
+              create: (_) => LoginController(),
+              lazy: false,
+            )
+          ],
+          routers: {
+            '/login': (context) => const LoginPage(),
+          },
+        );
+}
